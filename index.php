9a383826fcec915105c5a727948b5f0cc36e5340
sanitating <97023669+sanitating@users.noreply.github.com>
2022-03-29 18:49:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,189 @@
+<!doctype html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+    <?php include("includes/include_css.html") ?>
+</head>
+
+<body>
+    <?php include("includes/browser_upgrade.html") ?>
+    <?php include("includes/loader.html") ?>
+    <?php include("includes/header.html") ?>
+    <?php include("includes/sidebar.html") ?>
+
+    <main id="index_page" class="smooth_scroll_pages">
+        <!--  insert body content  -->
+        <section class="bef-banner">
+            <div class="container">
+                <div class="option ">
+                    <ul class="flex">
+                        <li>Home/</li>
+                        <li>Shop/</li>
+                        <li>Bloom</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="banner">
+                <div class="image">
+                    <img src="../img/banner.jpg" alt="" class="img-responsive">
+                </div>
+            </div>
+        </section>
+
+        <section class="text-box">
+            <div class="container">
+                <div class="heading">
+                    <h1>Bridal Lehangas</h1>
+                </div>
+                <div class="para">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                        of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                        but also the leap into electronic typesetting, remaining essentially unchanged. It was
+                        popularised in the 1960s with the release.</p>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="tabs">
+            <div class="container">
+                <select name="subject" id="subject" class="common">
+                    <option value="" selected="selected">Category</option>
+                </select>
+
+                <select name="topic" id="topic" class="common">
+                    <option value="" selected="selected">Filters</option>
+                </select>
+
+                <select name="chapter" id="chapter" class="common different">
+                    <option value="" selected="selected">Sort:Newest</option>
+                </select>
+            </div>
+        </section>
+
+
+        <section class="img-grid">
+            <div class="container">
+                <div class="grid-box">
+
+                    <div class="grid">
+                        <img src="../img/design1.jpg" alt="" class="img-responsive">
+                        <img src="../img/heart.png" alt="" class="img-responsive heart">
+                        <p>Canary Yellow Organza Lehenga</p>
+                        <p>₹550,000</p>
+
+                        <div class="arrow">
+                            <div class="know-btn">Know More</div>
+                            <img src="../img/arrow.jpg" alt="">
+                        </div>
+                    </div>
+
+                    <div class="grid">
+                        <img src="../img/design2.jpg" alt="" class="img-responsive">
+                        <img src="../img/heart.png" alt="" class="img-responsive heart">
+                        <p>Canary Yellow Organza Lehenga</p>
+                        <p>₹550,000</p>
+                        <div class="arrow">
+                            <div class="know-btn">Know More</div>
+                            <img src="../img/arrow.jpg" alt="">
+                        </div>
+                    </div>
+
+                    <div class="grid">
+                        <img src="../img/design3.jpg" alt="" class="img-responsive">
+                        <img src="../img/heart.png" alt="" class="img-responsive heart">
+                        <p>Canary Yellow Organza Lehenga</p>
+                        <p>₹550,000</p>
+                        <div class="arrow">
+                            <div class="know-btn">Know More</div>
+                            <img src="../img/arrow.jpg" alt="">
+                        </div>
+                    </div>
+
+                    <div class="grid">
+                        <img src="../img/design4.jpg" alt="" class="img-responsive">
+                        <img src="../img/heart.png" alt="" class="img-responsive heart">
+                        <p>Canary Yellow Organza Lehenga</p>
+                        <p>₹550,000</p>
+                        <div class="arrow">
+                            <div class="know-btn">Know More</div>
+                            <img src="../img/arrow.jpg" alt="">
+                        </div>
+                    </div>
+
+                    <div class="grid">
+                        <img src="../img/design5.jpg" alt="" class="img-responsive">
+                        <img src="../img/heart.png" alt="" class="img-responsive heart">
+                        <p>Canary Yellow Organza Lehenga</p>
+                        <p>₹550,000</p>
+                        <div class="arrow">
+                            <div class="know-btn">Know More</div>
+                            <img src="../img/arrow.jpg" alt="">
+                        </div>
+                    </div>
+
+                    <div class="grid">
+                        <img src="../img/design6.jpg" altclass="img-responsive">
+                        <img src="../img/heart.png" alt="" class="img-responsive heart">
+                        <p>Canary Yellow Organza Lehenga</p>
+                        <p>₹550,000</p>
+                        <div class="arrow">
+                            <div class="know-btn">Know More</div>
+                            <img src="../img/arrow.jpg" alt="">
+                        </div>
+                    </div>
+
+                    <div class="grid">
+                        <img src="../img/design1.jpg" altclass="img-responsive">
+                        <img src="../img/heart.png" alt="" class="img-responsive heart">
+                        <p>Canary Yellow Organza Lehenga</p>
+                        <p>₹550,000</p>
+                        <div class="arrow">
+                            <div class="know-btn">Know More</div>
+                            <img src="../img/arrow.jpg" alt="">
+                        </div>
+                    </div>
+
+                    <div class="grid">
+                        <img src="../img/design2.jpg" alclass="img-responsive">
+                        <img src="../img/heart.png" alt="" class="img-responsive heart">
+                        <p>Canary Yellow Organza Lehenga</p>
+                        <p>₹550,000</p>
+                        <div class="arrow">
+                            <div class="know-btn">Know More</div>
+                            <img src="../img/arrow.jpg" alt="">
+                        </div>
+                    </div>
+
+                    <div class="grid">
+                        <img src="../img/design3.jpg" aclass="img-responsive">
+                        <img src="../img/heart.png" alt="" class="img-responsive heart">
+                        <p>Canary Yellow Organza Lehenga</p>
+                        <p>₹550,000</p>
+                        <div class="arrow">
+                            <div class="know-btn">Know More</div>
+                            <img src="../img/arrow.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+        <!--  end body content -->
+    </main>
+    <?php include("includes/include_js.html") ?>
+
+</body>
+
+</html>
---
+++ product_inside.php
@@ -0,0 +1,242 @@
+<!doctype html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+    <?php include("includes/include_css.html") ?>
+</head>
+
+<body>
+    <?php include("includes/browser_upgrade.html") ?>
+    <?php include("includes/loader.html") ?>
+    <?php include("includes/header.html") ?>
+    <?php include("includes/sidebar.html") ?>
+
+    <main id="product_inside_page" class="smooth_scroll_pages">
+        <!--  insert body content  -->
+
+
+        <section class="grid_box">
+            <div class="container">
+                <div class="details">
+
+                    <div class="grid-layout">
+                        <div class="grid-tab">
+
+
+                            <div class="grid-img comm">
+                                <img src="img/box-img1.jpg" alt="" class="img-responsive ">
+                            </div>
+
+                            <div class="grid-img comm">
+                                <img src="img/box-img2.jpg" alt="" class="img-responsive">
+                            </div>
+
+                            <div class="grid-img comm">
+                                <img src="img/box-img3.jpg" alt="" class="img-responsive">
+                            </div>
+
+                            <div class="grid-img">
+                                <img src="img/box-img4.jpg" alt="" class="img-responsive">
+                            </div>
+                        </div>
+
+                        <div class="grid-img5">
+                            <img src="img/box-img-5.jpg" alt="" class="img-responsive">
+                        </div>
+                    </div>
+
+                    <div class="dense">
+                        <div class="heading">
+                            <h1>Dense Red Raw Silk Lehenga</h1>
+                            <p>₹555,000</p>
+                            <ul>
+                                <li>Dense red raw silk lehenga, metaculiously embroidered with our badla embroidery</li>
+                                <li>Matched with a preplum flair blouse adorned in baadla work and badla embroidered
+                                    blouse
+                                </li>
+                                <li>Dense red net dupatta with badla embroidery</li>
+                                <li>Skirt: Comes with a cancan</li>
+                            </ul>
+                        </div>
+
+
+                        <div class="select-flex">
+                            <p>Select Size:</p>
+                            <p class="para box">XS</p>
+                            <p class="para com">S</p>
+                            <p class="para com">M</p>
+                            <p class="para com">L</p>
+                            <p class="para com">XL</p>
+                        </div>
+
+
+                        <div class="enq">
+                            <div class="arr">
+                                <a href="">Enquire now</a>
+                                <img src="img/pink-arrow.png" alt="">
+                            </div>
+
+                            <div class="share">
+                                <a href="">Share</a>
+                                <img src="" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <div class="para-box">
+            <p class="border">Product Description</p>
+            <p>Product Care Info</p>
+            <p>FAQs</p>
+        </div>
+
+        <section class="description">
+            <div class="container">
+                <div class="text-para">
+                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
+                        classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a
+                        Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin
+                        words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
+                        classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32
+                        and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero,
+                        written in 45 BC. This book is a treatise on the theory of ethics, very popular during the
+                        Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
+                        section 1.10.32</p>
+                    <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
+                        Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced
+                        in their exact original form, accompanied by English versions from the 1914 translation by H.
+                        Rackham.</p>
+                </div>
+            </div>
+        </section>
+
+
+
+        <section class="rainbow">
+            <div class="container">
+                <div class="color-flex">
+                    <div class="color_box">
+                        <div class="boxing">
+                            <h1 class="sust">Sustainable Style</h1>
+                            <p>All our creations can be passed on from generation to generation, remaining as relevant
+                                as ever.</p>
+                        </div>
+
+                        <div class="boxing">
+                            <h1 class="wom">Women Empowerment </h1>
+                            <p>An extremely skilled community of artisans work relentlessly to make you standout on your
+                                big day.</p>
+                        </div>
+
+                        <div class="boxing">
+                            <h1 class="leg">Legacy of Luxury</h1>
+                            <p>Pick from our rich and opulent garments that transcend trends, letting you gracefully
+                                express your style. </p>
+                        </div>
+
+                        <div class="boxing">
+                            <h1 class="love">Pre-loved Fashion</h1>
+                            <p>A part of our inventory goes to an orphanage, where children use fabrics to make bags and
+                                clothes. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="img-grid">
+            <div class="container">
+                <div class="grid-box tab_slider">
+
+                    <div class="grid">
+                        <img src="../img/design1.jpg" alt="" class="img-responsive">
+                        <img src="../img/heart.png" alt="" class="img-responsive heart">
+                        <p>Canary Yellow Organza Lehenga</p>
+                        <p>₹550,000</p>
+
+                        <div class="arrow">
+                            <div class="know-btn">Know More</div>
+                            <img src="../img/arrow.jpg" alt="">
+                        </div>
+                    </div>
+
+                    <div class="grid">
+                        <img src="../img/design2.jpg" alt="" class="img-responsive">
+                        <img src="../img/heart.png" alt="" class="img-responsive heart">
+                        <p>Canary Yellow Organza Lehenga</p>
+                        <p>₹550,000</p>
+                        <div class="arrow">
+                            <div class="know-btn">Know More</div>
+                            <img src="../img/arrow.jpg" alt="">
+                        </div>
+                    </div>
+
+                    <div class="grid">
+                        <img src="../img/design3.jpg" aclass="img-responsive">
+                        <img src="../img/heart.png" alt="" class="img-responsive heart">
+                        <p>Canary Yellow Organza Lehenga</p>
+                        <p>₹550,000</p>
+                        <div class="arrow">
+                            <div class="know-btn">Know More</div>
+                            <img src="../img/arrow.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- <script>
+        $('.tab_slider').slick({
+            dots: true,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        }); -->
+        </script>
+
+        <!--  end body content -->
+    </main>
+    <?php include("includes/include_js.html") ?>
+
+</body>
+
+</html>
